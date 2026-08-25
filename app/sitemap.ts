970f19080fdf8d11c25750dff45638944537a339
ibrahimmunshi2003 <ibrahimmunshi2003@gmail.com>
2026-08-25 20:46:50 +0530

    fix sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from "next";
+﻿import type { MetadataRoute } from "next";
 
 import { siteConfig } from "@/lib/site";
 import { getCategories } from "@/lib/categories";
